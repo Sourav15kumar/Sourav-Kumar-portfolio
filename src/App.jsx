35fad93ff77a648620ddd9c76816f0bf444466b4
sourav kumar <souravkumar152004@gmail.com>
2025-08-07 20:05:49 +0530

    Removed unused component and fixed broken import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Notfound from './pages/Notfound'
-import component from './component'
+
 
 
 
